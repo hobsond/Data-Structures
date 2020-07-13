80d8193bf7a48b8d953fb537265574dd4907c575
hobsond <60270270+hobsond@users.noreply.github.com>
2020-07-13 18:36:30 -0400

    completed the list with an array as its storage

--- stack/stack.py
+++ stack/stack.py
@@ -13,13 +13,20 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
+        self.storage = []
         # self.storage = ?
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        self.storage.append(value)
+        self.size = len((self.storage))
 
     def pop(self):
-        pass
+        if self.size is 0:
+            return None
+        else:
+            pop = self.storage.pop()
+            self.size = len(self.storage)
+            return pop
